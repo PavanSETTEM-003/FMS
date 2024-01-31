6e19998c11998e7cb2ca1f76d6a4f8132fb0f2a0
Pavan Kumar Settem <88257205+PavanSETTEM-003@users.noreply.github.com>
2024-01-31 13:33:05 +0530

    conversion to FastAPI

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, request, jsonify
 from datetime import datetime
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
 from pymongo import MongoClient
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")
-app = Flask(__name__)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 def connect_database():
     try:
@@ -16,15 +22,18 @@ def connect_database():
     except Exception as e:
         print(f"Unable to connect to the database: {e}")
 
+connect_database()
 
-@app.route('/')
-def home():
-    return render_template('index.html', name='Flask')
 
-@app.route('/insert_expense', methods=['POST'])
-def insert_expense():
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post('/insert_expense')
+async def insert_expense(request: Request):
     try:
-        expenses = request.form.to_dict()
+        expensess_received = await request.form()
+        expenses = {key: value for key, value in expensess_received.items()}
         print(expenses)
 
         current_date = datetime.now().strftime("%d-%m-%Y")
@@ -39,38 +48,35 @@ def insert_expense():
                     "Expenses": [expenses]}
             app.collection.insert_one(document)
 
-        return jsonify({'message': 'Added Sucessfully'})
-    except Exception as error:
-        return jsonify({'message': f'Unsuccessfully -- {error}'})
-
-@app.route("/insert_task", methods = ['POST'])
-def insert_task():
-    tasks_received = request.form.to_dict()
-    print(tasks_received)
-
-    current_date = datetime.now().strftime("%d-%m-%Y")
-    existing_document = app.collection.find_one({"Date": current_date})
-
-    if existing_document:
-        # Update existing document by adding new tasks
-        app.collection.update_one({"Date": current_date}, {"$set": {"Tasks": tasks_received}})
-
-    else:
-        # Insert a new document with the specified date and tasks
-        document = {
-            "Date": current_date,
-            "Tasks": tasks_received
-        }
-        app.collection.insert_one(document)
-
-    return jsonify({'star_count': len(tasks_received)})
-
-
-
-if __name__ == '__main__':
-    try:
-        connect_database()
-        app.run(debug=True, port=5000)
+        return ({'message': 'Added Sucessfully'})
     except Exception as error:
         print(error)
-        
+        return ({'message': f'Unsuccessfully -- {error}'})
+
+@app.post("/insert_task", response_model=dict)
+async def insert_task(request: Request):
+    try:
+        tasks_received = await request.form()
+        tasks_dict = {key: value for key, value in tasks_received.items()}
+        print(tasks_dict)
+
+        current_date = datetime.now().strftime("%d-%m-%Y")
+        existing_document = app.collection.find_one({"Date": current_date})
+
+        if existing_document:
+            # Update existing document by adding new tasks
+            app.collection.update_one({"Date": current_date}, {"$set": {"Tasks": tasks_dict}})
+        else:
+            # Insert a new document with the specified date and tasks
+            document = {
+                "Date": current_date,
+                "Tasks": tasks_dict
+            }
+            app.collection.insert_one(document)
+
+        return {'star_count': len(tasks_dict)}
+    except Exception as error:
+        print(error)
+        raise HTTPException(status_code=500, detail=f"Unsuccessfully -- {error}")
+
+
