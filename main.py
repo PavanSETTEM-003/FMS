7b8fde5408e41e5d6ee9923bc65c035c4dd52670
Pavan Kumar Settem <pavankumar.settem@gmail.com>
2024-02-27 23:56:50 +0530

    Dynamic amount refresh

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def connect_database():
     try:
         app.mongodb_client = MongoClient(config["mongodb_connection_string"])
         app.database = app.mongodb_client[config["DB_NAME"]]
-        app.collection = app.database[config["collection_name"]]
+        app.Tracker_Collection = app.database[config["Tracker_collection_name"]]
+        app.Summary_Collection = app.database[config["Summary_collection_name"]]
 
         print("Connected to the MongoDB database!")
     except Exception as e:
@@ -24,34 +25,81 @@ def connect_database():
 
 connect_database()
 
+def Update_Expenses_Summary(current_date, expenses):
+    try:
+        current_year,current_month = str(current_date[6::]),str(current_date[3:5])
+
+        month_expenses = app.Summary_Collection.find_one({"year."+current_year : {"$exists" : True}})
+
+        if(month_expenses == None):
+            app.Summary_Collection.update_one(
+                { "id": "65dda2f121377b9e345a32c6"},
+                { "$set": { "year."+current_year: [] } })
+            
+            month_expenses = app.Summary_Collection.find_one({"year."+current_year : {"$exists" : True}})
+
+        month_expenses_list = month_expenses["year"][current_year]
+
+        if(len(month_expenses_list) == int(current_month)):
+            month_expenses_list[-1] += int(expenses["price"])
+
+        else:
+            month_expenses_list.append(int(expenses["price"]))
+
+        app.Summary_Collection.update_one({"year." + current_year: {"$exists": True}},
+                      {"$set": {"year." + current_year: month_expenses_list}})
+
+        return int(month_expenses_list[-1])
+
+    except Exception as error:
+        print("Func_'Update_Expenses_Summary' :",error)
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/fetch_amount")
+async def fetch_amount(request: Request):
+    try:
+        current_year = str(datetime.now().strftime("%d-%m-%Y"))[6::]      
+        month_expenses = app.Summary_Collection.find_one({"year."+current_year : {"$exists" : True}})
+
+        if(month_expenses == None):
+            return({"amount_this_month":0})
+        
+        else:
+            return ({"amount_this_month":month_expenses["year"][current_year][-1]})
+
+    except Exception as error:
+        print(error)
+        return ({"amount_this_month":"uanble to fetch"})
 
 @app.post('/insert_expense')
 async def insert_expense(request: Request):
     try:
         expensess_received = await request.form()
         expenses = {key: value for key, value in expensess_received.items()}
-        print(expenses)
+        # print(expenses)
 
         current_date = datetime.now().strftime("%d-%m-%Y")
-        Exists = app.collection.find_one({"Date": current_date})
+        Date_Exists = app.Tracker_Collection.find_one({"Date": current_date})
 
-        if(Exists):
-            app.collection.update_one({"Date": current_date}, {"$push": {"Expenses": expenses}})
+        amount_this_month = Update_Expenses_Summary(current_date, expenses)   ## updating the Summary_Collection of the expenses
+
+        if(Date_Exists):
+            app.Tracker_Collection.update_one({"Date": current_date}, {"$push": {"Expenses": expenses}})
         
         else:
             document = {
                     "Date": current_date,
                     "Expenses": [expenses]}
-            app.collection.insert_one(document)
+            app.Tracker_Collection.insert_one(document)
 
-        return ({'message': 'Added Sucessfully'})
+        return ({'message': 'Added Sucessfully', 'amount_this_month': amount_this_month})
+    
     except Exception as error:
         print(error)
-        return ({'message': f'Unsuccessfully -- {error}'})
+        return ({'message': f'Unsuccessfully -- {error}', 'amount_this_month': "Unable to fetch"})
 
 @app.post("/insert_task", response_model=dict)
 async def insert_task(request: Request):
@@ -61,22 +109,20 @@ async def insert_task(request: Request):
         print(tasks_dict)
 
         current_date = datetime.now().strftime("%d-%m-%Y")
-        existing_document = app.collection.find_one({"Date": current_date})
+        existing_document = app.Tracker_Collection.find_one({"Date": current_date})
 
         if existing_document:
             # Update existing document by adding new tasks
-            app.collection.update_one({"Date": current_date}, {"$set": {"Tasks": tasks_dict}})
+            app.Tracker_Collection.update_one({"Date": current_date}, {"$set": {"Tasks": tasks_dict}})
         else:
             # Insert a new document with the specified date and tasks
             document = {
                 "Date": current_date,
                 "Tasks": tasks_dict
             }
-            app.collection.insert_one(document)
+            app.Tracker_Collection.insert_one(document)
 
         return {'star_count': len(tasks_dict)}
     except Exception as error:
         print(error)
         raise HTTPException(status_code=500, detail=f"Unsuccessfully -- {error}")
-
-
